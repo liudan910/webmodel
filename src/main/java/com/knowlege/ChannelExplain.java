a8e100ec0e117e1ae213aa7acfce79eb010f25f5
liudan93 <liudan19@jd.com>
2017-03-04 17:05:18 +0800

    nio服务端

--- src/main/java/com/knowlege/ChannelExplain.java
+++ src/main/java/com/knowlege/ChannelExplain.java
@@ -1,33 +1,30 @@
 package com.knowlege;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
 import static java.lang.System.out;
 public class ChannelExplain {
-    private static void main(String[] args) throws IOException {
-        SocketChannel socketChannel = SocketChannel.open();
-        socketChannel.configureBlocking(false);
-        socketChannel.connect(new InetSocketAddress("http://jenkov.com",80));
-        long startTime = System.currentTimeMillis();
-        int cont=0;
-        while (!socketChannel.finishConnect()){
-            cont++;
-            if((System.currentTimeMillis()-startTime)/1000 == cont){
-                out.print("-----------");
+    public static void main(String[] args) throws IOException {
+        InetSocketAddress socketAddress = new InetSocketAddress("localhost", 5555);
+        ServerSocketChannel server = ServerSocketChannel.open();
+        server.socket().bind(socketAddress);
+        SocketChannel channel = null;
+        while((channel=server.accept())!=null){
+            out.println("--------");
+            OutputStream out = channel.socket().getOutputStream();
+            while(true){
+                out.write("hello world!!".getBytes());
+                out.flush();
             }
         }
-        ByteBuffer buf = ByteBuffer.allocate(48);
-        int bytesRread = socketChannel.read(buf);
-        socketChannel.close();
     }
 }
 
 
 
 /*
-1  FileChannel 不可以设置非阻塞模式
-2.SocketChannel
- 可以设置 SocketChannel 为非阻塞模式（non-blocking mode）.设置之后，就可以在异步模式下调用connect(), read() 和write()了。
+服务端：ServerSocketChannel
  */
